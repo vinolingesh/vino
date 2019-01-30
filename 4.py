53caf604e6e7ec888623be052f9ec08ea27975ff
vinolingesh <47170505+vinolingesh@users.noreply.github.com>
2019-01-30 14:32:15 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+a=input()
+alpha=["a","A","E","e","i","I","o","O","u","U","b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N","p","P","q","Q","r","R","s","S","t","T","v","V","w","W","x","X","y","Y","z","Z"]
+if ( a in alpha):
+    print("Alphabet")
+else:
+    print("No")
+
